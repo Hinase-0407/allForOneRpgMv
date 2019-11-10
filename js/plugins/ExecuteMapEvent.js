1aa60f78a094f3ceb33f2b4f965a82ddbfaf3303
nashie <secret@icloud.com>
2019-11-10 17:28:57 +0900

    起動に必要なJSファイルを追加.

--- js/plugins/ExecuteMapEvent.js
+++ js/plugins/ExecuteMapEvent.js
@@ -5,9 +5,10 @@
 /*:
  * @plugindesc マップイベントを実行するプラグイン.
  * @author hinase
+ * @varsion 1.0.0
  *
- * @help
- *
+ * @help プラグインコマンドにこのプラグイン名を記入することで発動します。呼び出すイベントのメモ欄に"areaId"というメタデータを記入が必要です。
+ * @memo 1.0.0 マップイベントクラス作成. 呼び出し処理追加. ウィンドウ開閉処理追加.(author by.hinase)
  */
 
 (function() {
@@ -23,7 +24,13 @@
     /**
      * イベント用のウィンドウを表示.
      */
-    MapEvent.prototype.openWindow = function() {};
+    MapEvent.prototype.openWindow = function() {
+        var areaId = this.areaId;
+
+        // マップの背景表示.
+        // 更地の場合は、エリアIDを参照に描画.
+        // 施設がある場合は、施設IDを参照に描画.
+    };
 
     /**
      * マップイベントを実施.
@@ -44,14 +51,18 @@
 
         // ここからマップイベント発生のための処理を記入
         if (command === 'ExecuteMapEvent') {
-            // イベント発動マスのエリアIDを取得
-            var areaId = this.character(0).event().meta.areaId;
+            // メタ情報が登録されていない場合
+            if (!this.character(0).event().meta) $gameMessage.add("このイベントのメモ欄に情報がありません。\nメモ欄に「areaId」を記入するか、\n「ExecuteMap」の呼び出し処理を削除してください。\n");
+            else {
+                // イベント発動マスのエリアIDを取得
+                var areaId = this.character(0).event().meta.areaId;
 
-            console.log(this.character(0).event().meta);
-            $gameMessage.add(areaId);
-            // var event = new MapEvent(areaId);
+                if (!areaId) $gameMessage.add("エリアIDが設定されていません。\n\n\n");
+                $gameMessage.add(areaId);
+                // var event = new MapEvent(areaId);
 
-            // event.openWindow();
+                // event.openWindow();
+            }
         }
     }
 
