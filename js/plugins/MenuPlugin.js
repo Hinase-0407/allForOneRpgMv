aed63976b0f0afca592e713b49fd923543973e72
fd3s0412 <fd3s0412@gmail.com>
2019-12-28 14:44:30 +0900

    MapPlugin追加

---
+++ js/plugins/MenuPlugin.js
@@ -0,0 +1,27 @@
+(function () {
+    Window_Base.prototype.drawActorSimpleStatus = function(actor, x, y, width) {
+        var lineHeight = this.lineHeight();
+        var x2 = x + 180;
+        var width2 = Math.min(200, width - 180 - this.textPadding());
+        this.drawActorName(actor, x, y);
+        this.drawActorLevel(actor, x, y + lineHeight * 1);
+        this.drawActorIcons(actor, x, y + lineHeight * 2);
+        this.drawActorClass(actor, x2, y);
+        this.drawActorHp(actor, x2, y + lineHeight * 1, width2);
+        this.drawActorMp(actor, x2, y + lineHeight * 2, width2);
+    };
+
+    Window_Base.prototype.drawActorIcons = function(actor, x, y, width) {
+        width = width || 144;
+        // console.log(actor.allIcons())
+        var icons = actor.allIcons().slice(0, Math.floor(width / Window_Base._iconWidth));
+        for (var i = 0; i < icons.length; i++) {
+            this.drawIcon(icons[i], x + Window_Base._iconWidth * i, y + 2);
+        }
+        this.drawCharacter("Actor2", 2, x - 80, y + 2);
+    };
+
+    Window_MenuStatus.prototype.maxItems = function() {
+        return window.client.;
+    };
+})();
